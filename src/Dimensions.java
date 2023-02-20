d5818ec2937b389ab99621509d1ec7a2e33f5a36
Lewandowski <adam.lewandowski@dxc.com>
2023-02-20 10:13:14 +0100

    Dimesions added

---
+++ src/Dimensions.java
@@ -0,0 +1,10 @@
+public class Dimensions {
+    private int x;
+    private int y;
+    Dimensions (int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public int getX(){ return x; }
+    public int getY(){ return y; }
+}
