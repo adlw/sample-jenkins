c1b9b4934fcf42422623acf0f217989daa2697dd
Lewandowski <adam.lewandowski@dxc.com>
2023-02-20 11:39:04 +0100

    New feture visible

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("New dimesnions");
         Warehouse stock = new Warehouse();
+        System.out.println("Stock");
     }
 }
