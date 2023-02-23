446b109d65a3d038d20f23398f8b0c017cb1ff3d
Lewandowski <adam.lewandowski@dxc.com>
2023-02-23 11:10:50 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("New dimesnions");
 //        Dimensions room = new Dimensions(40, 50);
+        System.out.println("Update");
     }
 }
